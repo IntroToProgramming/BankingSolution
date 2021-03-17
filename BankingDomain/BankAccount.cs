66cb0f7a2fd8a8784d9da8350b3b4c00f44199e0
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-17 10:33:05 -0400

    we tried. kinda sucks

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -2,9 +2,11 @@
 
 namespace BankingDomain
 {
+    public enum AccountType {  Standard, Gold }
     public class BankAccount
     {
         private decimal _balance = 5000; // Class Variable "Field"
+        public AccountType AccountType = AccountType.Standard;
         public decimal GetBalance()
         {
             return _balance;
@@ -12,7 +14,12 @@ namespace BankingDomain
 
         public void Deposit(decimal amountToDeposit)
         {
-            _balance += amountToDeposit;
+            decimal bonus = 0;
+            if(AccountType == AccountType.Gold)
+            {
+                bonus = amountToDeposit * .10M;
+            }
+            _balance += amountToDeposit + bonus;
         }
 
         public void Withdraw(decimal amountToWithdraw)
